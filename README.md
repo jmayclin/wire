45246d7785a3f1374c359bb726b7511e483b29d7
James Mayclin <maycj@amazon.com>
2025-07-22 02:54:22 +0000

    docs: add alert and getting started to readme

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 `brass-aphid-wire` is a crate to simplify the parsing of TLS records.
 
+> [!CAUTION]
+> This is very much "in-progress", and is not at all suitable for production. Only TLS 1.3 is supported with the 3 main AEAD ciphers. Generally the library will simply explode/panic if you go off this narrow, happy path. I do intend to eventually make this a production-suitable library, but for now it's best restricted to fiddling about.
+
+# Getting Started.
+
+The two main bits of the public API are the `KeyManager` and the `DecryptingPipe`.
+
+`KeyManager` provides that the key logging callback which can be associated with a config like OpenSSL's [`SslContextBuilder::set_keylog_callback`](https://docs.rs/openssl/latest/openssl/ssl/struct.SslContextBuilder.html#method.set_keylog_callback) or s2n-tls's [`Config::set_key_log_callback`](https://docs.rs/s2n-tls/latest/s2n_tls/config/struct.Builder.html#method.set_key_log_callback).
+
+`DecryptingPipe` will wrap some type `T` which implements `Read` and `Write`. This is designed to work with interfaces like OpenSSL's [`SslStream`](https://docs.rs/openssl/latest/openssl/ssl/struct.SslStream.html). If you were previously using `SslStream<TcpStream>`, then you'd swap out the inner `TcpStream` to use the `DecryptingPipe`, yielding an `SslStream<DecryptingPipe<TcpStream>>`.
+
+There aren't any super great examples of this right now, but the best places to look would be
+- `brass-aphid-wire/src/bin/decrypting_client.rs`: This file contains a basic TLS client which will print out the full transcript of it's handshake against an arbitrary TLS endpoint.
+- `brass-aphid-wire/src/testing/openssl_decrypted_transcript.rs`: Sets up a test decrypting with an OpenSSL client and an OpenSSL server.
+- `brass-aphid-wire/src/testing/s2n_decrypted_transcript.rs`: Sets up a test decrypting with an s2n-tls client and an s2n-tls server.
+
+# Background
+
 I love snooping. It's one of my favorite activities, but it's hard to do with TLS.
 
 Basically the only option (that I'm aware of) is to use wireshark. This requires you to
--- brass-aphid-wire/src/bin/decrypting_client.rs
+++ brass-aphid-wire/src/bin/decrypting_client.rs
@@ -7,12 +7,16 @@ const DOMAIN: &str = "www.amazon.com";
 const PORT: u16 = 443;
 
 fn main() -> anyhow::Result<()> {
+    // first we create a KeyManager. Most TLS implementation will set the key-logging
+    // callback per-config. The KeyManager provides that callback.
     let key_manager = KeyManager::new();
-    let key_manager_handle = key_manager.clone();
-
+    
     let client_config = {
+        let key_manager_handle = key_manager.clone();
         let mut builder = SslContext::builder(SslMethod::tls_client())?;
         builder.set_keylog_callback(move |sslref, key_log| {
+            // move, so the config stores its own copy of the KeyManager, which is
+            // internally reference counted.
             key_manager_handle.parse_key_log_line(key_log.as_bytes());
         });
         builder.build()
@@ -20,20 +24,29 @@ fn main() -> anyhow::Result<()> {
 
     let client = Ssl::new(&client_config)?;
 
+    // configure the "normal" transport stream.
     let client_stream = std::net::TcpStream::connect(format!("{DOMAIN}:{PORT}")).unwrap();
     client_stream.set_read_timeout(Some(Duration::from_secs(1)));
+
+    // wrap the transport stream in the "decrypting pipe", associating the key manager
+    // from before with the decrypting pipe.
     let decrypting_pipe = DecryptingPipe::new(key_manager, client_stream);
-    let decrypter = decrypting_pipe.decrypter.transcript.clone();
+
+    // hold a reference to the transcript, which we can look at after the handshake
+    // (or during the handshake)
+    let transcript = decrypting_pipe.decrypter.transcript.clone();
+
+    // construct the actual SslStream.
     let mut stream = SslStream::new(client, decrypting_pipe).unwrap();
     stream.connect().unwrap();
+    // this read is necessary to make the client read in the "secret" data that the server
+    // sends after the handshake (which is the NST).
+    // We set the read_timeout earlier so that this doesn't just hand forever.
     stream.read(&mut []);
-    let shutdown_state = stream.shutdown().unwrap();
+    stream.shutdown().unwrap();
 
-    let trace = format!("{:#?}", *decrypter.lock().unwrap());
-    std::fs::write(DOMAIN, trace);
+    let transcript_trace = format!("{:#?}", *transcript.lock().unwrap());
+    std::fs::write(DOMAIN, transcript_trace).unwrap();
 
-    // disable cert validation because we don't actually care if an endpoint is good
-
-    println!("hello world");
     Ok(())
 }
